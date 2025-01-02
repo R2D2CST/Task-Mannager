e6caa07596e9515d3a375a5ce396cd5f60791147
R2D2CST <r2d2cst@gmail.com>
2025-01-02 10:06:04 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
 # Native python libraries
+from datetime import datetime
 
 # Third party libraries
 
 # Self build libraries
 from Constants.Constants import WEEKEND_DAYS, ENGLISH_WEEKEND
+from Func.Human.Responsible import Responsible
+from Func.Tasks.Task import Task
 
 print(WEEKEND_DAYS)
 print(ENGLISH_WEEKEND)
+
+worker = Responsible("Arturo", "Castella")
+
+dueDate = datetime(2025, 1, 15)
+
+Task_1 = Task(
+    description="New Task",
+    responsible=worker,
+    dueDate=dueDate,
+    activity="Program a task manager",
+)
+
+print(dueDate)
+print(worker)
+print(Task_1)
